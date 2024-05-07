8bbe0aaa20647ffaf949c24f7942e785c2a53816
Thorsten Lorenz <thlorenz@gmx.de>
2024-05-07 14:28:27 -0500

    chore: include program id with pda seeds

--- addresses/src/consts.rs
+++ addresses/src/consts.rs
@@ -1,6 +1,8 @@
 // NOTE: originally created in delegation program:
 // https://github.com/magicblock-labs/delegation-program/blob/104d7772882e5fbeb871f90a1a33a95ccf98a62c/src/consts.rs
 
+use solana_sdk::pubkey::Pubkey;
+
 /// The seed of the authority account PDA.
 pub const DELEGATION: &[u8] = b"delegation";
 
@@ -14,4 +16,13 @@ pub const STATE_DIFF: &[u8] = b"state-diff";
 pub const COMMIT_RECORD: &[u8] = b"commit-state-record";
 
 /// The address of the delegation program
-pub const DELEGATION_PROGRAM_ADDR: &str = "DELeGGvXpWV2fqJUhqcF5ZSYMS4JTLjteaAMARRSaeSh";
+pub const DELEGATION_PROGRAM_ADDR: &str =
+    "DELeGGvXpWV2fqJUhqcF5ZSYMS4JTLjteaAMARRSaeSh";
+
+pub const DELEGATION_PROGRAM_ARRAY: [u8; 32] = [
+    181, 183, 0, 225, 242, 87, 58, 192, 204, 6, 34, 1, 52, 74, 207, 151, 184,
+    53, 6, 235, 140, 229, 25, 152, 204, 98, 126, 24, 147, 128, 167, 62,
+];
+
+pub const DELEGATION_PROGRAM_ID: Pubkey =
+    Pubkey::new_from_array(DELEGATION_PROGRAM_ARRAY);
--- addresses/src/pda.rs
+++ addresses/src/pda.rs
@@ -7,23 +7,23 @@ macro_rules! seeds {
     ($prefix:ident, $bytes_const:expr) => {
         paste! {
             #[allow(clippy::needless_lifetimes)]
-            pub fn [<$prefix _seeds>]<'a>(id: &'a [u8]) -> [&'a [u8]; 2] {
-                [$bytes_const, id]
+            pub fn [<$prefix _seeds>]<'a>(pda_id: &'a [u8]) -> [&'a [u8]; 3] {
+                [&crate::consts::DELEGATION_PROGRAM_ARRAY, $bytes_const, pda_id]
             }
             #[allow(clippy::needless_lifetimes)]
-            pub fn [<$prefix _seeds_with_bump>]<'a>(id: &'a [u8], bump: &'a [u8; 1]) -> [&'a [u8]; 3] {
-                [$bytes_const, id, bump]
+            pub fn [<$prefix _seeds_with_bump>]<'a>(pda_id: &'a [u8], bump: &'a [u8; 1]) -> [&'a [u8]; 4] {
+                [&crate::consts::DELEGATION_PROGRAM_ARRAY, $bytes_const, pda_id, bump]
             }
             #[allow(clippy::needless_lifetimes)]
-            pub fn [<$prefix _seeds_from_pubkey>]<'a>(id: &'a ::solana_sdk::pubkey::Pubkey) -> [&'a [u8]; 2] {
-                [$bytes_const, id.as_ref()]
+            pub fn [<$prefix _seeds_from_pubkey>]<'a>(pda_id: &'a ::solana_sdk::pubkey::Pubkey) -> [&'a [u8]; 3] {
+                [&crate::consts::DELEGATION_PROGRAM_ARRAY, $bytes_const, pda_id.as_ref()]
             }
             #[allow(clippy::needless_lifetimes)]
             pub fn [<$prefix _seeds_with_bump_from_pubkey>]<'a>(
-                id: &'a ::solana_sdk::pubkey::Pubkey,
+                pda_id: &'a ::solana_sdk::pubkey::Pubkey,
                 bump: &'a [u8; 1],
-            ) -> [&'a [u8]; 3] {
-                [$bytes_const, id.as_ref(), bump]
+            ) -> [&'a [u8]; 4] {
+                [&crate::consts::DELEGATION_PROGRAM_ARRAY, $bytes_const, pda_id.as_ref(), bump]
             }
         }
     };
@@ -38,7 +38,9 @@ seeds! {commit_record, b"commit-state-record" }
 mod tests {
     use solana_sdk::pubkey::Pubkey;
 
-    use crate::consts::{BUFFER, COMMIT_RECORD, DELEGATION, STATE_DIFF};
+    use crate::consts::{
+        BUFFER, COMMIT_RECORD, DELEGATION, DELEGATION_PROGRAM_ARRAY, STATE_DIFF,
+    };
 
     use super::*;
 
@@ -49,7 +51,7 @@ mod tests {
     fn test_delegation_seeds() {
         let id = [1, 2, 3];
         let seeds = delegation_seeds(&id);
-        assert_eq!(seeds, [DELEGATION, &id]);
+        assert_eq!(seeds, [&DELEGATION_PROGRAM_ARRAY, DELEGATION, &id]);
     }
 
     #[test]
@@ -57,14 +59,14 @@ mod tests {
         let id = [1, 2, 3];
         let bump = [4];
         let seeds = delegation_seeds_with_bump(&id, &bump);
-        assert_eq!(seeds, [DELEGATION, &id, &bump]);
+        assert_eq!(seeds, [&DELEGATION_PROGRAM_ARRAY, DELEGATION, &id, &bump]);
     }
 
     #[test]
     fn test_delegation_seeds_from_pubkey() {
         let id = Pubkey::new_unique();
         let seeds = delegation_seeds_from_pubkey(&id);
-        assert_eq!(seeds, [DELEGATION, id.as_ref()]);
+        assert_eq!(seeds, [&DELEGATION_PROGRAM_ARRAY, DELEGATION, id.as_ref()]);
     }
 
     #[test]
@@ -72,7 +74,10 @@ mod tests {
         let id = Pubkey::new_unique();
         let bump = [4];
         let seeds = delegation_seeds_with_bump_from_pubkey(&id, &bump);
-        assert_eq!(seeds, [DELEGATION, id.as_ref(), &bump]);
+        assert_eq!(
+            seeds,
+            [&DELEGATION_PROGRAM_ARRAY, DELEGATION, id.as_ref(), &bump]
+        );
     }
 
     // -----------------
@@ -82,7 +87,7 @@ mod tests {
     fn test_buffer_seeds() {
         let id = [1, 2, 3];
         let seeds = buffer_seeds(&id);
-        assert_eq!(seeds, [BUFFER, &id]);
+        assert_eq!(seeds, [&DELEGATION_PROGRAM_ARRAY, BUFFER, &id]);
     }
 
     #[test]
@@ -90,14 +95,14 @@ mod tests {
         let id = [1, 2, 3];
         let bump = [4];
         let seeds = buffer_seeds_with_bump(&id, &bump);
-        assert_eq!(seeds, [BUFFER, &id, &bump]);
+        assert_eq!(seeds, [&DELEGATION_PROGRAM_ARRAY, BUFFER, &id, &bump]);
     }
 
     #[test]
     fn test_buffer_seeds_from_pubkey() {
         let id = Pubkey::new_unique();
         let seeds = buffer_seeds_from_pubkey(&id);
-        assert_eq!(seeds, [BUFFER, id.as_ref()]);
+        assert_eq!(seeds, [&DELEGATION_PROGRAM_ARRAY, BUFFER, id.as_ref()]);
     }
 
     #[test]
@@ -105,7 +110,10 @@ mod tests {
         let id = Pubkey::new_unique();
         let bump = [4];
         let seeds = buffer_seeds_with_bump_from_pubkey(&id, &bump);
-        assert_eq!(seeds, [BUFFER, id.as_ref(), &bump]);
+        assert_eq!(
+            seeds,
+            [&DELEGATION_PROGRAM_ARRAY, BUFFER, id.as_ref(), &bump]
+        );
     }
 
     // -----------------
@@ -115,7 +123,7 @@ mod tests {
     fn test_state_diff_seeds() {
         let id = [1, 2, 3];
         let seeds = state_diff_seeds(&id);
-        assert_eq!(seeds, [STATE_DIFF, &id]);
+        assert_eq!(seeds, [&DELEGATION_PROGRAM_ARRAY, STATE_DIFF, &id]);
     }
 
     #[test]
@@ -123,14 +131,14 @@ mod tests {
         let id = [1, 2, 3];
         let bump = [4];
         let seeds = state_diff_seeds_with_bump(&id, &bump);
-        assert_eq!(seeds, [STATE_DIFF, &id, &bump]);
+        assert_eq!(seeds, [&DELEGATION_PROGRAM_ARRAY, STATE_DIFF, &id, &bump]);
     }
 
     #[test]
     fn test_state_diff_seeds_from_pubkey() {
         let id = Pubkey::new_unique();
         let seeds = state_diff_seeds_from_pubkey(&id);
-        assert_eq!(seeds, [STATE_DIFF, id.as_ref()]);
+        assert_eq!(seeds, [&DELEGATION_PROGRAM_ARRAY, STATE_DIFF, id.as_ref()]);
     }
 
     #[test]
@@ -138,7 +146,10 @@ mod tests {
         let id = Pubkey::new_unique();
         let bump = [4];
         let seeds = state_diff_seeds_with_bump_from_pubkey(&id, &bump);
-        assert_eq!(seeds, [STATE_DIFF, id.as_ref(), &bump]);
+        assert_eq!(
+            seeds,
+            [&DELEGATION_PROGRAM_ARRAY, STATE_DIFF, id.as_ref(), &bump]
+        );
     }
 
     // -----------------
@@ -148,7 +159,7 @@ mod tests {
     fn test_commit_record_seeds() {
         let id = [1, 2, 3];
         let seeds = commit_record_seeds(&id);
-        assert_eq!(seeds, [COMMIT_RECORD, &id]);
+        assert_eq!(seeds, [&DELEGATION_PROGRAM_ARRAY, COMMIT_RECORD, &id]);
     }
 
     #[test]
@@ -156,14 +167,20 @@ mod tests {
         let id = [1, 2, 3];
         let bump = [4];
         let seeds = commit_record_seeds_with_bump(&id, &bump);
-        assert_eq!(seeds, [COMMIT_RECORD, &id, &bump]);
+        assert_eq!(
+            seeds,
+            [&DELEGATION_PROGRAM_ARRAY, COMMIT_RECORD, &id, &bump]
+        );
     }
 
     #[test]
     fn test_commit_record_seeds_from_pubkey() {
         let id = Pubkey::new_unique();
         let seeds = commit_record_seeds_from_pubkey(&id);
-        assert_eq!(seeds, [COMMIT_RECORD, id.as_ref()]);
+        assert_eq!(
+            seeds,
+            [&DELEGATION_PROGRAM_ARRAY, COMMIT_RECORD, id.as_ref()]
+        );
     }
 
     #[test]
@@ -171,6 +188,9 @@ mod tests {
         let id = Pubkey::new_unique();
         let bump = [4];
         let seeds = commit_record_seeds_with_bump_from_pubkey(&id, &bump);
-        assert_eq!(seeds, [COMMIT_RECORD, id.as_ref(), &bump]);
+        assert_eq!(
+            seeds,
+            [&DELEGATION_PROGRAM_ARRAY, COMMIT_RECORD, id.as_ref(), &bump]
+        );
     }
 }
