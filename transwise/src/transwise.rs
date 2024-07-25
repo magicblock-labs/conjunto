3bba96e410ed2bb12c7b1479ce03c0b849559cc6
Vincent Brunet <vincent.brunet.us@gmail.com>
2024-07-25 19:19:55 +0900

    nit-rename

--- transwise/src/transwise.rs
+++ transwise/src/transwise.rs
@@ -17,7 +17,7 @@ use crate::{
 /// Guiding decisions are made by consulting the state of accounts on chain
 /// See [../examples/guiding_transactions.rs] for more info.
 pub struct Transwise {
-    account_lock_state_provider: AccountChainSnapshotProvider<
+    account_chain_snapshot_provider: AccountChainSnapshotProvider<
         RpcAccountProvider,
         DelegationRecordParserImpl,
     >,
@@ -25,12 +25,12 @@ pub struct Transwise {
 
 impl Transwise {
     pub fn new(config: RpcProviderConfig) -> Self {
-        let account_lock_state_provider = AccountChainSnapshotProvider::<
+        let account_chain_snapshot_provider = AccountChainSnapshotProvider::<
             RpcAccountProvider,
             DelegationRecordParserImpl,
         >::new(config);
         Self {
-            account_lock_state_provider,
+            account_chain_snapshot_provider,
         }
     }
 
@@ -45,7 +45,7 @@ impl Transwise {
     ) -> TranswiseResult<TransactionAccountMetas> {
         TransactionAccountMetas::from_versioned_transaction(
             tx,
-            &self.account_lock_state_provider,
+            &self.account_chain_snapshot_provider,
         )
         .await
     }
@@ -61,7 +61,7 @@ impl Transwise {
     ) -> TranswiseResult<TransactionAccountMetas> {
         TransactionAccountMetas::from_sanitized_transaction(
             tx,
-            &self.account_lock_state_provider,
+            &self.account_chain_snapshot_provider,
         )
         .await
     }
@@ -75,7 +75,7 @@ impl Transwise {
     ) -> TranswiseResult<TransactionAccountMetas> {
         TransactionAccountMetas::from_accounts_holder(
             accounts,
-            &self.account_lock_state_provider,
+            &self.account_chain_snapshot_provider,
         )
         .await
     }
