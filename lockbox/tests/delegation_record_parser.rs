9c8a3e3fcecad5f2a0f829954ea8be97ab6fdde7
Gabriele Picco <piccogabriele@gmail.com>
2024-12-17 12:12:18 +0100

    chore: update to latest delegation program

--- lockbox/tests/delegation_record_parser.rs
+++ lockbox/tests/delegation_record_parser.rs
@@ -3,7 +3,6 @@ use conjunto_core::{
     delegation_record_parser::DelegationRecordParser,
 };
 use conjunto_lockbox::delegation_record_parser_impl::DelegationRecordParserImpl;
-use dlp::state::account::AccountWithDiscriminator;
 use solana_sdk::pubkey;
 
 #[test]
@@ -11,19 +10,18 @@ fn test_delegation_record_parser() {
     // NOTE: from delegation-program/tests/fixtures/accounts.rs
     let authority = pubkey!("CLMS5guJDje8BA9tQdd1wXmGmPx5S32yhGztw4xytAYN");
     let owner = pubkey!("CLMS5guJDje8BA9tQdd1wXmGmPx5S32yhGztw4xytAYN");
-    let delegation_record_account_data: Vec<u8> = [
-        &dlp::state::DelegationRecord::discriminator().to_bytes(),
-        dlp::state::DelegationRecord::to_bytes(&dlp::state::DelegationRecord {
-            authority,
-            owner,
-            delegation_slot: 4,
-            commit_frequency_ms: 30_000,
-            lamports: 500,
-        }),
-    ]
-    .concat();
+    let mut data = [0u8; size_of::<DelegationRecord>() + 8];
+    dlp::state::DelegationRecord {
+        authority,
+        owner,
+        delegation_slot: 4,
+        commit_frequency_ms: 30_000,
+        lamports: 500,
+    }
+    .to_bytes_with_discriminator(&mut data)
+    .unwrap();
     let parser = DelegationRecordParserImpl;
-    let record = parser.try_parse(&delegation_record_account_data).unwrap();
+    let record = parser.try_parse(&data).unwrap();
     assert_eq!(
         record,
         DelegationRecord {
